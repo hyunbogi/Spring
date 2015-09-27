2053bf2cc7bd1b4366e2df9d3034eee72112b0b6
Hyunbok Lee <hyunbogi@gmail.com>
2015-09-28 03:13:01 +0900

    Refactoring
    1. User 생성자 추가
    2. 테스트 코드 추가

--- src/main/java/com/hyunbogi/spring/dao/UserDao.java
+++ src/main/java/com/hyunbogi/spring/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.hyunbogi.spring.dao;
 
 import com.hyunbogi.spring.model.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -38,16 +39,21 @@ public class UserDao {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
 
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
-
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
         rs.close();
         ps.close();
         c.close();
+
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
 
         return user;
     }
--- src/test/java/com/hyunbogi/spring/dao/UserDaoTest.java
+++ src/test/java/com/hyunbogi/spring/dao/UserDaoTest.java
@@ -4,6 +4,8 @@ import com.hyunbogi.spring.model.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
@@ -16,21 +18,55 @@ public class UserDaoTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("kmalloc", "Hyunbok Lee", "aaa12345");
+        User user2 = new User("iu", "Jieun Lee", "aaa12345");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user1 = new User();
-        user1.setId("kmalloc");
-        user1.setName("Hyunbok Lee");
-        user1.setPassword("aaa12345");
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        User userGet1 = dao.get(user1.getId());
+        assertThat(userGet1.getName(), is(user1.getName()));
+        assertThat(userGet1.getPassword(), is(user1.getPassword()));
+
+        User userGet2 = dao.get(user2.getId());
+        assertThat(userGet2.getName(), is(user2.getName()));
+        assertThat(userGet2.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void getUserFailure() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.get("unknown_id");
+    }
+
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("tang", "Taeyeon Kim", "aaa12345");
+        User user2 = new User("iu", "Jieun Lee", "aaa12345");
+        User user3 = new User("bong", "Boyoung Park", "aaa12345");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
         dao.add(user1);
         assertThat(dao.getCount(), is(1));
 
-        User user2 = dao.get(user1.getId());
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        assertThat(user2.getName(), is(user1.getName()));
-        assertThat(user2.getPassword(), is(user1.getPassword()));
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 }
