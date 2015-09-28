cbd157bc084285be286aee5f1c29dd05315ce7ad
Hyunbok Lee <hyunbogi@gmail.com>
2015-09-29 01:47:41 +0900

    Refactoring
     - Spring의 JdbcTemplate을 이용하도록 UserDao 클래스 수정

--- src/main/java/com/hyunbogi/spring/dao/UserDao.java
+++ src/main/java/com/hyunbogi/spring/dao/UserDao.java
@@ -1,84 +1,42 @@
 package com.hyunbogi.spring.dao;
 
 import com.hyunbogi.spring.model.User;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "INSERT INTO users (id, name, password) VALUES (?, ?, ?)"
+        jdbcTemplate.update(
+                "INSERT INTO users (id, name, password) VALUES (?, ?, ?)",
+                user.getId(),
+                user.getName(),
+                user.getPassword()
         );
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
     }
 
     public User get(String id) throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "SELECT * FROM users WHERE id = ?"
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM users WHERE id = ?",
+                new Object[]{id},
+                (rs, rowNum) -> {
+                    return new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+                }
         );
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-        if (rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-        }
-        rs.close();
-        ps.close();
-        c.close();
-
-        if (user == null) {
-            throw new EmptyResultDataAccessException(1);
-        }
-
-        return user;
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("DELETE FROM users");
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        jdbcTemplate.update("DELETE FROM users");
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM users");
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return count;
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
     }
 }
