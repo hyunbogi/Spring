6b9bd5a0a892cc7e1315dd57be88a1d9775978a0
Hyunbok Lee <hyunbogi@gmail.com>
2015-10-04 03:06:31 +0900

    Refactoring
     - 트랜잭션이 적용되는 UserService를 프록시로 생성하도록 변경
    
     - 프록시가 스프링의 FactoryBean을 통해 생성되도록 변경

--- src/test/java/com/hyunbogi/spring/service/UserServiceTest.java
+++ src/test/java/com/hyunbogi/spring/service/UserServiceTest.java
@@ -4,7 +4,7 @@ import com.hyunbogi.spring.dao.UserDao;
 import com.hyunbogi.spring.mock.MockMailSender;
 import com.hyunbogi.spring.model.Level;
 import com.hyunbogi.spring.model.User;
-import com.hyunbogi.spring.service.tx.UserServiceTx;
+import com.hyunbogi.spring.service.tx.TransactionHandler;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -102,9 +103,16 @@ public class UserServiceTest {
         testUserService.setUserDao(userDao);
         testUserService.setMailSender(mailSender);
 
-        UserServiceTx txUserService = new UserServiceTx();
-        txUserService.setTransactionManager(transactionManager);
-        txUserService.setUserService(testUserService);
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(testUserService);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern("upgradeLevels");
+
+        UserService txUserService = (UserService) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{UserService.class},
+                txHandler
+        );
 
         userDao.deleteAll();
         users.forEach(userDao::add);
