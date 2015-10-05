cab97d380e5d69a1b2d386eb735eec1b93ddaa03
Hyunbok Lee <hyunbogi@gmail.com>
2015-10-06 05:07:45 +0900

    데이터베이스 접속 정보를 외부의 프로퍼티 파일로 분리

--- src/main/java/com/hyunbogi/spring/context/AppContext.java
+++ src/main/java/com/hyunbogi/spring/context/AppContext.java
@@ -1,10 +1,12 @@
 package com.hyunbogi.spring.context;
 
-import com.mysql.jdbc.Driver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -13,19 +15,33 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "com.hyunbogi.spring")
 @Import(SqlServiceContext.class)
+@PropertySource("/props/database.properties")
 public class AppContext {
+    @Value("${db.driverClass}")
+    private Class<? extends Driver> driverClass;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost/springtestdb");
-        dataSource.setUsername("hyunbogi");
-        dataSource.setPassword("aaa12345");
+        dataSource.setDriverClass(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         return dataSource;
     }
@@ -44,5 +60,10 @@ public class AppContext {
         mailSender.setHost("mail.email.com");
 
         return mailSender;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
